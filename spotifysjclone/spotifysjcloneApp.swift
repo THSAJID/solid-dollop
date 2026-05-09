186e06b7f67be3ef8d2bbb58cc4acf48e408bae4
Tamzid Hasan Sajid <iamthsajid@gmail.com>
2026-05-09 12:43:44 +0600

    Initial Commit

---
+++ spotifysjclone/spotifysjcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifysjcloneApp.swift
+//  spotifysjclone
+//
+//  Created by TH Sajid on 9/5/26.
+//
+
+import SwiftUI
+
+@main
+struct spotifysjcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
